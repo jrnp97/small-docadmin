7d1e3f0203cfcbcf690dcda6470a0134ba88251e
jrnp97 <jrnp1997@gmail.com>
2018-05-05 21:53:51 -0500

    Creating app with twig

---
+++ with_twig/audiologia.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jaime
+ * Date: 5/5/2018
+ * Time: 6:52 AM
+ */
+
+require 'vendor/autoload.php';
+
+$loader = new Twig_Loader_Filesystem('views');
+$twig = new Twig_Environment($loader);
+
+
+try {
+    echo $twig->render('audiologia.html.twig', array(
+        'title_form' => 'Historia Audiologia',
+        'body_ident' => json_decode(file_get_contents("views/forms/jsons/historia_audiologia/identificacion.json")),
+        'body_ana' => json_decode(file_get_contents("views/forms/jsons/historia_audiologia/anamnesis.json")),
+        'body_ant_f' => json_decode(file_get_contents("views/forms/jsons/historia_audiologia/ante_familia.json")),
+        'body_ant_o' => json_decode(file_get_contents("views/forms/jsons/historia_audiologia/ant_otro.json")),
+        'body_expo' => json_decode(file_get_contents("views/forms/jsons/historia_audiologia/exposicion.json")),
+        'body_actual' => json_decode(file_get_contents("views/forms/jsons/historia_audiologia/estado_actual.json"))
+    ));
+} catch (Twig_Error_Loader $e) {
+} catch (Twig_Error_Runtime $e) {
+} catch (Twig_Error_Syntax $e) {
+}
---
+++ with_twig/audiometria.php
@@ -0,0 +1,18 @@
+<?php
+
+require 'vendor/autoload.php';
+
+$loader = new Twig_Loader_Filesystem('views');
+$twig = new Twig_Environment($loader);
+
+try{
+    echo $twig->render('audiometria.html.twig', array(
+        'title_form' => 'AUDIOMETRIA',
+        'info_body' => json_decode(file_get_contents('views/forms/jsons/audiometria/information.json')),
+        'inter_body' => json_decode(file_get_contents('views/forms/jsons/audiometria/interpretaciones.json')),
+        'decla_body' => json_decode(file_get_contents('views/forms/jsons/audiometria/declarations.json'))
+    ));
+} catch (Twig_Error_Loader $e) {
+} catch (Twig_Error_Runtime $e) {
+} catch (Twig_Error_Syntax $e) {
+}
