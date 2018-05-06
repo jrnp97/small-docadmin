d07500148fdc1e37c2e7a565530e336a5ddbd2f6
jrnp97 <jrnp1997@gmail.com>
2018-05-06 15:46:13 -0500

    Init build 'historia ocupacional' form

---
+++ with_twig/ocupacional.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jaime
+ * Date: 5/6/2018
+ * Time: 2:25 PM
+ */
+
+require 'vendor/autoload.php';
+
+$loader = new Twig_Loader_Filesystem('views');
+
+$twig = new Twig_Environment($loader);
+
+try
+{
+    echo $twig->render('h_ocupacional.html.twig',array(
+        'title_form' => 'Historia Ocupacional',
+        'body_ide' => json_decode(file_get_contents('views/forms/jsons/historia_ocupacional/identificacion.json')),
+        'body_a_ocu' => json_decode(file_get_contents('views/forms/jsons/historia_ocupacional/ant_ocupacionales.json'))
+    ));
+}catch (Twig_Error_Runtime $e){
+}catch (Twig_Error_Loader $e){
+}catch (Twig_Error_Syntax $e){
+}
