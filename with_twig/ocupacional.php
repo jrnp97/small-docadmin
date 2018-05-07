7cb601139ac17b7dabad8d4b6013f8a1c1c553d0
jrnp97 <jrnp1997@gmail.com>
2018-05-07 15:31:00 -0500

    Init form historia ocupacional

--- with_twig/ocupacional.php
+++ with_twig/ocupacional.php
@@ -16,7 +16,13 @@ try
 {
     echo $twig->render('h_ocupacional.html.twig',array(
         'title_form' => 'Historia Ocupacional',
-        'body_a_pat' => json_decode(file_get_contents('views/forms/jsons/historia_ocupacional/ant_patologicos.json'))
+        'body_ident' => json_decode(file_get_contents('views/forms/jsons/historia_ocupacional/identificacion.json')),
+        'body_a_ocu' => json_decode(file_get_contents('views/forms/jsons/historia_ocupacional/ant_ocupacionales.json')),
+        'body_a_pat' => json_decode(file_get_contents('views/forms/jsons/historia_ocupacional/ant_patologicos.json')),
+        'body_hab' => json_decode(file_get_contents('views/forms/jsons/historia_ocupacional/habitos.json')),
+        'body_a_gin' => json_decode(file_get_contents('views/forms/jsons/historia_ocupacional/ant_gineco.json')),
+        'body_e_fi' => json_decode(file_get_contents('views/forms/jsons/historia_ocupacional/examen_fisico.json')),
+        'body_conclu' => json_decode(file_get_contents('views/forms/jsons/historia_ocupacional/conclusiones.json'))
     ));
 }catch (Twig_Error_Runtime $e){
 }catch (Twig_Error_Loader $e){
