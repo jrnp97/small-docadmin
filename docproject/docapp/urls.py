e9775653f5e43248875cfd27c85ff32155c131aa
dgun17 <t00038310@utbvirtual.edu.co>
2018-06-27 17:24:55 -0500

    Integring menu and dashboard

--- docproject/docapp/urls.py
+++ docproject/docapp/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url
-from docapp.views import example
+from docapp import views
 
 
 app_name = 'docapp'
 urlpatterns =[
-    url(r'^example/$', example, name='example')
+    url(r'^/$', views.index, name='index'),
+    url(r'^login/$', views.login, name='login')
 ]
+
+# url(r'^login/$', views.Login.as_view(), name='login')
--- docproject/docapp/views.py
+++ docproject/docapp/views.py
@@ -1,10 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.urlresolvers import reverse_lazy
+# from django.contrib.auth.views import LoginView
 
 # Create your views here.
 
 
-def example(request):
-    return render(request, 'base/base.html')
+def index(request):
+    return render(request, 'docapp/index.html')
 
 
+def login(request):
+    if request.method == 'POST':
+        return redirect('docapp:index')
+    else:
+        return render(request, 'docapp/login.html')
+
+
+# class Login(LoginView):
+#     template_name = 'docapp/login.html'
+#     success_url = reverse_lazy('docapp:index')
 
