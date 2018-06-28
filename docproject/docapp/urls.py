9878828a9c49cb1cb210ffdf7031c23c119f9ae4
dgun17 <t00038310@utbvirtual.edu.co>
2018-06-28 10:34:49 -0500

    Add templates behavior

--- docproject/docapp/urls.py
+++ docproject/docapp/urls.py
@@ -4,8 +4,16 @@ from docapp import views
 
 app_name = 'docapp'
 urlpatterns =[
-    url(r'^/$', views.index, name='index'),
-    url(r'^login/$', views.login, name='login')
+    url(r'^home/$', views.index, name='index'),
+    url(r'^login/$', views.login, name='login'),
+    url(r'^dashboard/$', views.dashboard, name='dashboard'),
+    url(r'^dashboard/ingreso/$', views.ingreso, name='ingreso'),
+    url(r'^dashboard/medicos/$', views.doctors, name='medicos'),
+    url(r'^dashboard/examenes/general', views.general, name='medicina_general'),
+    url(r'^dashboard/examenes/lab', views.laboratorio, name='laboratorio'),
+    url(r'^dashboard/examenes/audiometria', views.audiometria, name='audiometria'),
+    url(r'^dashboard/examenes/visiometria', views.visiometria, name='visiometria'),
+
 ]
 
 # url(r'^login/$', views.Login.as_view(), name='login')
--- docproject/docapp/views.py
+++ docproject/docapp/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render, redirect
-from django.core.urlresolvers import reverse_lazy
+# from django.core.urlresolvers import reverse_lazy
 # from django.contrib.auth.views import LoginView
 
 # Create your views here.
 
 
+# Simple views
 def index(request):
     return render(request, 'docapp/index.html')
+
+
+def dashboard(request):
+    context = dict({
+        'routes': ['Inicio'],
+        'page_header': 'Dashboard Proof',
+        'active': 'inicio',
+        'exam_collapse': True,
+    })
+    return render(request, 'docapp/home.html', context)
 
 
 def login(request):
@@ -14,6 +25,67 @@ def login(request):
         return redirect('docapp:index')
     else:
         return render(request, 'docapp/login.html')
+
+
+def ingreso(request):
+    context = dict({
+        'routes': ['Ingreso'],
+        'page_header': 'Dashboard Proof',
+        'active': 'ingreso',
+        'exam_collapse': True,
+    })
+    return render(request, 'docapp/ingreso.html', context)
+
+
+def doctors(request):
+    context = dict({
+        'routes': ['Medicos'],
+        'page_header': 'Dashboard Proof',
+        'active': 'doctors',
+        'exam_collapse': True,
+    })
+    return render(request, 'docapp/doctors.html', context)
+
+
+# Exams views
+def general(request):
+    context = dict({
+        'routes': ['Examenes', 'Medicina General'],
+        'page_header': 'Dashboard Proof',
+        'active': 'medicina_general',
+        'exam_collapse': False,
+    })
+    return render(request, 'docapp/exams/medicina_general.html', context)
+
+
+def laboratorio(request):
+    context = dict({
+        'routes': ['Examenes', 'Laboratorio'],
+        'page_header': 'Dashboard Proof',
+        'active': 'laboratorio',
+        'exam_collapse': False,
+    })
+    return render(request, 'docapp/exams/laboratorio.html', context)
+
+
+def visiometria(request):
+    context = dict({
+        'routes': ['Examenes', 'Visiometria'],
+        'page_header': 'Dashboard Proof',
+        'active': 'visiometria',
+        'exam_collapse': False,
+    })
+    return render(request, 'docapp/exams/visiometria.html', context)
+
+
+def audiometria(request):
+    context = dict({
+        'routes': ['Examenes', 'Audiometria'],
+        'page_header': 'Dashboard Proof',
+        'active': 'audiometria',
+        'exam_collapse': False,
+    })
+    return render(request, 'docapp/exams/audiometria.html', context)
 
 
 # class Login(LoginView):
